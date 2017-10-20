1a3cca659275a8cb4968dbc882191ba233c4a7d5
Minified <gora.pub@gmail.com>
2017-10-20 15:23:50 +0200

    cleanup, full commands for a01/z3

--- a01/home/a01.z3.sh
+++ a01/home/a01.z3.sh
@@ -1,11 +1,45 @@
 # 1)
 
-ffmpeg -i mowa.mp3 -f mp3 -ac 1 -acodec mp3 -q:a 2 a01.z3.1.mowa.mp3
+# v1:
+ffmpeg -i mowa.mp3 -y -ac 1 -f mp3 -acodec mp3 -q:a 0 a01.z3.1.v1.mowa.mp3
+ffmpeg -i mowa.mp3 -y -ac 1 -f mp3 -acodec mp3 -q:a 4 a01.z3.1.v1.mowa.mp3
+ffmpeg -i mowa.mp3 -y -ac 1 -f mp3 -acodec mp3 -q:a 9 a01.z3.1.v1.mowa.mp3
+
+ffmpeg -i muzyka.mp3 -y -ac 1 -f mp3 -acodec mp3 -q:a 0 a01.z3.1.v1.muzyka.mp3
+ffmpeg -i muzyka.mp3 -y -ac 1 -f mp3 -acodec mp3 -q:a 4 a01.z3.1.v1.muzyka.mp3
+ffmpeg -i muzyka.mp3 -y -ac 1 -f mp3 -acodec mp3 -q:a 9 a01.z3.1.v1.muzyka.mp3
+
+# v2:
+ffmpeg -i mowa.mp3 -y -ac 1 -codec:a libmp3lame -qscale:a 0 a01.z3.1.v2.quality0.mowa.mp3
+ffmpeg -i mowa.mp3 -y -ac 1 -codec:a libmp3lame -qscale:a 4 a01.z3.1.v2.quality4.mowa.mp3
+ffmpeg -i mowa.mp3 -y -ac 1 -codec:a libmp3lame -qscale:a 9 a01.z3.1.v2.quality9.mowa.mp3
+
+ffmpeg -i muzyka.mp3 -y -ac 1 -codec:a libmp3lame -qscale:a 0 a01.z3.1.v2.quality0.muzyka.mp3
+ffmpeg -i muzyka.mp3 -y -ac 1 -codec:a libmp3lame -qscale:a 4 a01.z3.1.v2.quality4.muzyka.mp3
+ffmpeg -i muzyka.mp3 -y -ac 1 -codec:a libmp3lame -qscale:a 9 a01.z3.1.v2.quality9.muzyka.mp3
 
 # 2)
 
-ffmpeg -i mowa.mp3 -f mp3 -ac 1 -acodec mp3 -b:a 320k 2 a01.z3.2.320k.mowa.mp3
+# v1:
+ffmpeg -i mowa.mp3 -y -ac 1 -f mp3 -acodec mp3 -b:a 8k 2 a01.z3.2.8k.mowa.mp3
+ffmpeg -i mowa.mp3 -y -ac 1 -f mp3 -acodec mp3 -b:a 128k 2 a01.z3.2.128k.mowa.mp3
+ffmpeg -i mowa.mp3 -y -ac 1 -f mp3 -acodec mp3 -b:a 320k 2 a01.z3.2.320k.mowa.mp3
+
+ffmpeg -i muzyka.mp3 -y -ac 1 -f mp3 -acodec mp3 -b:a 8k 2 a01.z3.2.8k.muzyka.mp3
+ffmpeg -i muzyka.mp3 -y -ac 1 -f mp3 -acodec mp3 -b:a 128k 2 a01.z3.2.128k.muzyka.mp3
+ffmpeg -i muzyka.mp3 -y -ac 1 -f mp3 -acodec mp3 -b:a 320k 2 a01.z3.2.320k.muzyka.mp3
+
+# v2:
+ffmpeg -i mowa.mp3 -y -ac 1 -codec:a libmp3lame -b:a 8k s01.z3.2.v2.br8k.mowa.mp3
+ffmpeg -i mowa.mp3 -y -ac 1 -codec:a libmp3lame -b:a 128k s01.z3.2.v2.br128k.mowa.mp3
+ffmpeg -i mowa.mp3 -y -ac 1 -codec:a libmp3lame -b:a 320k s01.z3.2.v2.br320k.mowa.mp3
+
+ffmpeg -i muzyka.mp3 -y -ac 1 -codec:a libmp3lame -b:a 8k s01.z3.2.v2.br8k.muzyka.mp3
+ffmpeg -i muzyka.mp3 -y -ac 1 -codec:a libmp3lame -b:a 128k s01.z3.2.v2.br128k.muzyka.mp3
+ffmpeg -i muzyka.mp3 -y -ac 1 -codec:a libmp3lame -b:a 320k s01.z3.2.v2.br320k.muzyka.mp3
 
 # 3)
 
-ffmpeg -i muzyka.mp3 -f wav - | lame --cbr -b 320 - a01.z3.3.b320.muzyka.mp3
+ffmpeg -i muzyka.mp3 -y -ac 1 -f wav - | lame --cbr -b 8 - a01.z3.3.b8k.muzyka.mp3
+ffmpeg -i muzyka.mp3 -y -ac 1 -f wav - | lame --cbr -b 160 - a01.z3.3.b160k.muzyka.mp3
+ffmpeg -i muzyka.mp3 -y -ac 1 -f wav - | lame --cbr -b 320 - a01.z3.3.b320k.muzyka.mp3
